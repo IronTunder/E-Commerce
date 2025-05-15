74f4255e240fcd9e6be7fa91bf7f98aa9efaeb33
IronTunder <matteoone75@gmail.com>
2025-05-16 00:06:27 +0200

    00:06 16/05/25

--- GestionePannelloRegistrazione.java
+++ GestionePannelloRegistrazione.java
@@ -1,7 +1,5 @@
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.io.*;
 import java.util.ArrayList;
 
@@ -10,132 +8,71 @@ public class GestionePannelloRegistrazione extends JPanel {
     JLabel email = new JLabel("Email: ");
     JLabel username = new JLabel("Username: ");
     JLabel password1 = new JLabel("Password: ");
-    JLabel password2 = new JLabel("Verifica Password");
+    JLabel password2 = new JLabel("Conferma Password: ");
 
     JTextField emailDaInserire = new JTextField(20);
     JTextField usernameDaInserire = new JTextField(20);
     JPasswordField password1DaInserire = new JPasswordField(20);
     JPasswordField password2DaInserire = new JPasswordField(20);
 
-    JCheckBox termini = new JCheckBox("Accetto termini di utilizzo");
+    JCheckBox termini = new JCheckBox("Accetto i termini di utilizzo");
 
-    JButton registrati = new JButton("Registrati");
-
-    String [] controlloEmail = {"@","@gmail.com","@gmail.it",".it",".com"};
-
-
-
-    public GestionePannelloRegistrazione(){
+    public GestionePannelloRegistrazione() {
         setLayout(new GridBagLayout());
+        setBackground(new Color(240, 248, 255));
 
         GridBagConstraints b = new GridBagConstraints();
+        b.insets = new Insets(10, 10, 10, 10);
 
         b.gridx = 0;
         b.gridy = 0;
-        b.insets = new Insets(10,10,10,10);
-        add(email,b);
+        add(email, b);
         b.gridx = 1;
-        b.gridy = 0;
-        add(emailDaInserire,b);
+        add(emailDaInserire, b);
 
         b.gridx = 0;
         b.gridy = 1;
-        add(username,b);
+        add(username, b);
         b.gridx = 1;
-        b.gridy = 1;
-        add(usernameDaInserire,b);
+        add(usernameDaInserire, b);
 
         b.gridx = 0;
         b.gridy = 2;
-        add(password1,b);
+        add(password1, b);
         b.gridx = 1;
-        b.gridy = 2;
-        add(password1DaInserire,b);
+        add(password1DaInserire, b);
 
         b.gridx = 0;
         b.gridy = 3;
-        add(password2,b);
+        add(password2, b);
         b.gridx = 1;
-        b.gridy = 3;
-        add(password2DaInserire,b);
+        add(password2DaInserire, b);
 
         b.gridx = 0;
         b.gridy = 4;
-        add(termini,b);
-
-        b.gridx = 1;
-        b.gridy = 5;
-        add(registrati,b);
-
-        registrati.setEnabled(false);
-
-
-        termini.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                if(termini.isSelected()){
-                    registrati.setEnabled(true);
-                }
-                else{
-                    registrati.setEnabled(false);
-                }
-            }
-        });
-
-
-        registrati.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                if(!(password1DaInserire.getText().equals(password2DaInserire.getText()))){
-                    JOptionPane.showMessageDialog(null,"Password non sono uguali");
-                }
-                else if (!(emailDaInserire.getText().contains("@") && (emailDaInserire.getText().contains("@gmail.com") || emailDaInserire.getText().contains(".it") || emailDaInserire.getText().contains(".com")))) {
-                    JOptionPane.showMessageDialog(null,"email non corretta");
-                }
-                else if(usernameDaInserire.getText().length() < 4 || usernameDaInserire.getText().length() > 20){
-                    JOptionPane.showMessageDialog(null,"Username non valido");
-                }
-                else{
-                    scrittura();
-                    JOptionPane.showMessageDialog(null,"Utente registrato con successo");
-                }
-            }
-        });
-
-
-        /*
-        CONTROLLI DA FARE:
-
-            - LA PASSWORD 1 DEVE ESSERE UGUALE ALLA PASSWORD 2
-            - FARE IL CONTROLLO DELL'EMAIL
-            - SE NON ACCETTA I TERMINI NON TI FA REGISTRARE
-         */
-
+        b.gridwidth = 2;
+        add(termini, b);
     }
 
-
-    public void scrittura(){
-        try {
+    public void scrittura() {
+        try (ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream("registrazione.dat"))) {
+            Persona p = new Persona(emailDaInserire.getText(),
+                    password1DaInserire.getPassword(),
+                    password2DaInserire.getPassword(),
+                    usernameDaInserire.getText());
             ArrayList<Persona> persone = lettura();
-            Persona p = new Persona(emailDaInserire.getText(),password1DaInserire.getPassword(),password2DaInserire.getPassword(),usernameDaInserire.getText());
             persone.add(p);
-            ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream("registrazione.dat"));
             out.writeObject(persone);
-        } catch (Exception e) {
-        }
-    }
-
-    public ArrayList<Persona> lettura(){
-        try {
-            ObjectInputStream inp = new ObjectInputStream(new FileInputStream("registrazione.dat"));
-            return (ArrayList<Persona>) inp.readObject();
         } catch (IOException e) {
-            return new ArrayList<>();
-        } catch (ClassNotFoundException e) {
-            return new ArrayList<>();
+            e.printStackTrace();
         }
     }
 
-
-
+    public ArrayList<Persona> lettura() {
+        try (ObjectInputStream inp = new ObjectInputStream(new FileInputStream("registrazione.dat"))) {
+            return (ArrayList<Persona>) inp.readObject();
+        } catch (IOException | ClassNotFoundException e) {
+            return new ArrayList<>();
+        }
+    }
 }
--- Login.java
+++ Login.java
@@ -9,51 +9,52 @@ public class Login extends JFrame implements ActionListener {
     JPasswordField passwordField = new JPasswordField();
     JButton login = new JButton("Accedi");
 
-    public Login(){
-        setDefaultCloseOperation(EXIT_ON_CLOSE);
+    public Login() {
+        super("Login");
+
+        setDefaultCloseOperation(DISPOSE_ON_CLOSE);
         setLocationRelativeTo(null);
-        setLayout(new BorderLayout());
+        setLayout(new BorderLayout(10, 10));
         setResizable(false);
 
-        JPanel nome = new JPanel();
-        JPanel password = new JPanel();
-        JPanel campi = new JPanel();
-
-        campi.setLayout(new GridLayout(2,1,10,10));
-
+        JPanel campi = new JPanel(new GridLayout(2, 1, 10, 10));
         TitledBorder nometitle = BorderFactory.createTitledBorder("Nome Utente / Email");
         TitledBorder passwtitle = BorderFactory.createTitledBorder("Password");
-
         nometitle.setTitleJustification(TitledBorder.CENTER);
         passwtitle.setTitleJustification(TitledBorder.CENTER);
 
-        nomeUtente.setPreferredSize(new Dimension(200,30));
-        passwordField.setPreferredSize(new Dimension(200,30));
-        nome.add(nomeUtente);
-        password.add(passwordField);
+        JPanel nome = new JPanel(new BorderLayout());
+        JPanel password = new JPanel(new BorderLayout());
         nome.setBorder(nometitle);
         password.setBorder(passwtitle);
+
+        nomeUtente.setPreferredSize(new Dimension(200, 30));
+        passwordField.setPreferredSize(new Dimension(200, 30));
+        nome.add(nomeUtente, BorderLayout.CENTER);
+        password.add(passwordField, BorderLayout.CENTER);
 
         campi.add(nome);
         campi.add(password);
 
-        add(campi,BorderLayout.CENTER);
+        add(campi, BorderLayout.CENTER);
+        login.setFont(new Font("Arial", Font.BOLD, 14));
+        login.setBackground(new Color(100, 149, 237));
+        login.setForeground(Color.WHITE);
+        add(login, BorderLayout.PAGE_END);
         login.addActionListener(this);
-        add(login,BorderLayout.PAGE_END);
 
         pack();
         setVisible(true);
     }
 
-    public static void main(String[] args) {
-        new Login();
-    }
-
     @Override
     public void actionPerformed(ActionEvent e) {
-        if(nomeUtente.getText().length() < 4 || nomeUtente.getText().length() > 20)
-            JOptionPane.showMessageDialog(this,"Inserire un nome utente valido");
-        else if (passwordField.getPassword().length < 8 || passwordField.getPassword().length > 20)
-            JOptionPane.showMessageDialog(this,"Inserire una password valida");
+        if (nomeUtente.getText().length() < 4 || nomeUtente.getText().length() > 20) {
+            JOptionPane.showMessageDialog(this, "Inserire un nome utente valido", "Errore", JOptionPane.ERROR_MESSAGE);
+        } else if (passwordField.getPassword().length < 8 || passwordField.getPassword().length > 20) {
+            JOptionPane.showMessageDialog(this, "Inserire una password valida", "Errore", JOptionPane.ERROR_MESSAGE);
+        } else {
+            JOptionPane.showMessageDialog(this, "Accesso effettuato con successo", "Successo", JOptionPane.INFORMATION_MESSAGE);
+        }
     }
 }
