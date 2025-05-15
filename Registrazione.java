1cb19b94fbee63faa4597208b3b790a7ac1cb16a
IronTunder <matteoone75@gmail.com>
2025-05-15 14:36:14 +0200

    1

---
+++ Registrazione.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+
+public class Registrazione extends JFrame {
+
+    public Registrazione(){
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setSize(500,400);
+        setLocationRelativeTo(null);
+        setResizable(false);
+
+
+        setVisible(true);
+    }
+}
