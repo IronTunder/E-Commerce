fc0f453479126619a30f9d00fbfbb21a2e0386ca
IronTunder <matteoone75@gmail.com>
2025-06-03 11:36:43 +0200

    03/06/25

--- src/view/carrello/InterfacciaCart.java
+++ src/view/carrello/InterfacciaCart.java
@@ -590,7 +590,6 @@ public class InterfacciaCart extends JFrame {
                             "</div></html>",
                     "Acquisto completato",
                     JOptionPane.INFORMATION_MESSAGE);
-
             updateCartDisplay();
             this.dispose();
         }
